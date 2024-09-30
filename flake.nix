b39b2e85e506d8e1c69c87be6d3b87a665764f52
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-09-30 09:29:32 +0200

    refactor: cleanup flake

--- flake.nix
+++ flake.nix
@@ -3,27 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixvim.url = "github:nix-community/nixvim";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
-    stylix.url = "github:danth/stylix";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     { nixvim, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.treefmt-nix.flakeModule
-        inputs.devenv.flakeModule
-      ];
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
-
       perSystem =
         {
-          config,
           pkgs,
           system,
           ...
@@ -33,15 +26,16 @@
           nixvim' = nixvim.legacyPackages.${system};
           nixvimModule = {
             inherit pkgs;
-            module = import ./config; # import the module directly
-            # You can use `extraSpecialArgs` to pass additional arguments to your module files
+            module = import ./config;
             extraSpecialArgs = {
-              # inherit (inputs) foo;
+              inherit inputs;
             };
           };
           nvim = nixvim'.makeNixvimWithModule nixvimModule;
         in
         {
+          formatter = pkgs.nixfmt-rfc-style;
+
           checks = {
             # Run `nix flake check .` to verify that your config is not broken
             default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
@@ -50,22 +44,6 @@
           packages = {
             # Lets you run `nix run .` to start nixvim
             default = nvim;
-          };
-
-          devenv.shells.default = {
-            packages = [
-              config.treefmt.build.wrapper
-            ];
-
-            # inputsFrom = [
-            # config.treefmt.build.shevShell
-            # ];
-          };
-
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs.nixfmt.enable = true;
-            programs.nixfmt.package = pkgs.nixfmt-rfc-style;
           };
         };
     };
