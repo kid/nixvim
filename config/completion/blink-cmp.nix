e9307c0a7051ca140fc76f02c77f9a909fbda1df
Arnaud Rebts <arnaud.rebts@gmail.com>
2024-10-12 12:52:59 +0200

    improve blink.cmp config

--- config/completion/blink-cmp.nix
+++ config/completion/blink-cmp.nix
@@ -1,6 +1,6 @@
 { localFlake, pkgs, ... }:
 {
-  # TODO: Disable Copilot's ghost text when completion menu is visible
+  # TODO: Disable Copilot's ghost text when completion menu is visible?
   # TODO: Use <C-y> for both Copilot and blink.cmp
 
   extraPlugins = [
@@ -16,13 +16,18 @@
       },
       keymap = {
         accept = '<C-y>',
-      },
-      documentation = {
-        auto_show = true,
+        -- NOTE: by default theses also incpude <Up> and <Down>, which then prevents from navigating Telescope's results
+        select_prev = '<C-p>',
+        select_next = '<C-n>',
       },
       fuzzy = {
         prebuiltBinaries = {
           download = false,
+        },
+      },
+      accept = {
+        auto_brackets = {
+          enabled = true,
         },
       },
       trigger = {
@@ -30,7 +35,32 @@
           enabled = true,
         },
       },
+      windows = {
+        autocomplete = {
+          draw = 'reversed',
+          min_width = 50,
+        },
+        documentation = {
+          auto_show = true,
+        },
+      },
       nerd_font_variant = 'mono',
     })
+
+    -- # FIXME: attempt to index global 'ev' (a nil value)
+    -- ev.on_load('copilot', function()
+    --   vim.schedule(function() 
+    --     local cmp = require('blink.cmp')
+    --
+    --     cmp.on_show(function()
+    --       require('copilot').dismiss()
+    --       vim.api.nvim_buf_set_var(0, 'copilot_suggestion_hidden', true)
+    --     end)
+    --
+    --     cmp.on_hide(function()
+    --       vim.api.nvim_buf_set_var(0, 'copilot_suggestion_hidden', false)
+    --     end)
+    --   end)
+    -- end)
   '';
 }
