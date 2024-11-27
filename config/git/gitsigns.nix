fa5a3d38c4b482988975459c0fb1b106630d5c6d
Arnaud_Rebts <arnaud.rebts@gmail.com>
2024-11-27 13:10:52 +0100

    Add some Gitsigns keymaps

--- config/git/gitsigns.nix
+++ config/git/gitsigns.nix
@@ -41,5 +41,85 @@ in
       '';
       options.desc = "Next hunk";
     }
+    {
+      mode = "n";
+      key = "<leader>gb";
+      action = helpers.mkRaw ''
+        function()
+          require('gitsigns').blame_line()
+        end
+      '';
+      options.desc = "Blame line";
+    }
+    {
+      mode = "n";
+      key = "<leader>gB";
+      action = helpers.mkRaw ''
+        function()
+          require('gitsigns').blame()
+        end
+      '';
+      options.desc = "Blame";
+    }
+    {
+      mode = "n";
+      key = "<leader>gs";
+      action = helpers.mkRaw ''
+        function()
+          require('gitsigns').stage_hunk()
+        end
+      '';
+      options.desc = "Stage hunk";
+    }
+    {
+      mode = "v";
+      key = "<leader>gs";
+      action = helpers.mkRaw ''
+        function()
+          require('gitsigns').stage_hunk({vim.fn.line('.'), vim.fn.line('v')})
+        end
+      '';
+      options.desc = "Stage hunk";
+    }
+    {
+      mode = "n";
+      key = "<leader>gr";
+      action = helpers.mkRaw ''
+        function()
+          require('gitsigns').reset_hunk()
+        end
+      '';
+      options.desc = "Reset hunk";
+    }
+    {
+      mode = "v";
+      key = "<leader>gr";
+      action = helpers.mkRaw ''
+        function()
+          require('gitsigns').reset_hunk({vim.fn.line('.'), vim.fn.line('v')})
+        end
+      '';
+      options.desc = "Reset hunk";
+    }
+    {
+      mode = "n";
+      key = "<leader>gS";
+      action = helpers.mkRaw ''
+        function()
+          require('gitsigns').stage_buffer()
+        end
+      '';
+      options.desc = "Stage buffer";
+    }
+    {
+      mode = "n";
+      key = "<leader>gu";
+      action = helpers.mkRaw ''
+        function()
+          require('gitsigns').undo_stage_hunk()
+        end
+      '';
+      options.desc = "Undo stage hunk";
+    }
   ];
 }
