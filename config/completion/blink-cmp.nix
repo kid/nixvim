7ab25ddf447ebea8ce9e8f143574d246f76b0e4e
Arnaud_Rebts <arnaud.rebts@gmail.com>
2025-02-11 11:53:49 +0100

    fix: use nixvim for configuring blink-cmp

--- config/completion/blink-cmp.nix
+++ config/completion/blink-cmp.nix
@@ -1,31 +1,18 @@
 {
-  localFlake,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  helpers = lib.nixvim;
-  opts = {
-    fuzy.prebuiltBinaries.download = false;
-    completion = {
-      accept.auto_brackets.enabled = true;
-      menu.min_width = 50;
-      documentation.auto_show = true;
+  plugins.blink-cmp = {
+    enable = true;
+    settings = {
+      completion = {
+        documentation = {
+          auto_show = true;
+        };
+        menu = {
+          min_width = 50;
+        };
+      };
+      signature = {
+        enable = true;
+      };
     };
-    signature.enabled = true;
   };
-in
-{
-  # TODO: Disable Copilot's ghost text when completion menu is visible?
-  # TODO: Use <C-y> for both Copilot and blink.cmp
-
-  extraPlugins = [
-    localFlake.inputs.blink-cmp.packages.${pkgs.system}.blink-cmp
-  ];
-
-  extraConfigLua = ''
-    local cmp = require("blink.cmp")
-    cmp.setup(${helpers.toLuaObject opts})
-  '';
 }
