9470f8e39ef99423082afceda4cdc14c37e87eea
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-02-28 17:07:52 +0100

    ci: attempt to trick renovate

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A nixvim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     nixvim = {
