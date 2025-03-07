7dfa52db6092288ebefed434abdc2fbb532c3d7f
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-03-07 20:32:31 +0100

    chore: remove github-actions, update flake

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-github-actions = {
-      url = "github:nix-community/nix-github-actions";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
