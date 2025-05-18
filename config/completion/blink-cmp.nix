c6eb53815e0868a978dd568b6487a383ed115489
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-05-18 11:01:33 +0200

    feat(completion): integrate `codecompanion` plugin and update `blink-cmp` configuration
    - Added `codecompanion` plugin with basic enablement in `codecompanion.nix`.
    - Updated `blink-cmp.nix` to include `codecompanion` as a source for per-filetype completion.

--- config/completion/blink-cmp.nix
+++ config/completion/blink-cmp.nix
@@ -3,45 +3,49 @@ let
   inherit (lib.nixvim) listToUnkeyedAttrs mkRaw;
 in
 {
-  plugins.blink-cmp = {
-    enable = true;
-    settings = {
-      completion = {
-        documentation = {
-          auto_show = true;
-        };
-        menu = {
-          min_width = 50;
-          draw = {
-            # We don't need label_description now because label and label_description are already
-            # combined together in label by colorful-menu.nvim.
-            columns = [
-              (listToUnkeyedAttrs [ "kind_icon" ])
-              ((listToUnkeyedAttrs [ "label" ]) // { gap = 1; })
-            ];
+  plugins = {
+    blink-cmp = {
+      enable = true;
+      settings = {
+        sources.per_filetype.codecompanion = [ "codecompanion" ];
 
-            components = {
-              label = {
-                text = mkRaw ''
-                  function (ctx)
-                    return require("colorful-menu").blink_components_text(ctx)
-                  end
-                '';
-                highlight = mkRaw ''
-                  function (ctx)
-                    return require("colorful-menu").blink_components_highlight(ctx)
-                  end
-                '';
+        completion = {
+          documentation = {
+            auto_show = true;
+          };
+          menu = {
+            min_width = 50;
+            draw = {
+              # We don't need label_description now because label and label_description are already
+              # combined together in label by colorful-menu.nvim.
+              columns = [
+                (listToUnkeyedAttrs [ "kind_icon" ])
+                ((listToUnkeyedAttrs [ "label" ]) // { gap = 1; })
+              ];
+
+              components = {
+                label = {
+                  text = mkRaw ''
+                    function (ctx)
+                      return require("colorful-menu").blink_components_text(ctx)
+                    end
+                  '';
+                  highlight = mkRaw ''
+                    function (ctx)
+                      return require("colorful-menu").blink_components_highlight(ctx)
+                    end
+                  '';
+                };
               };
             };
           };
         };
-      };
-      signature = {
-        enabled = true;
+        signature = {
+          enabled = true;
+        };
       };
     };
-  };
 
-  plugins.colorful-menu.enable = true;
+    colorful-menu.enable = true;
+  };
 }
---
+++ config/completion/codecompanion.nix
@@ -0,0 +1,5 @@
+{
+  plugins.codecompanion = {
+    enable = true;
+  };
+}
