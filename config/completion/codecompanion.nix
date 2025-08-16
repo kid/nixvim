563138a4cebb5736ca104cbaba8e828d8ef5ec1b
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-08-16 11:53:38 +0200

    ci: add check workflow (#41)
    * ci: add check workflow
    
    * fix: specify action palette provider for codecompanion
    
    * fix: disable checks for codecompanion for now

--- config/completion/codecompanion.nix
+++ config/completion/codecompanion.nix
@@ -1,5 +1,14 @@
+{ pkgs, ... }:
 {
   plugins.codecompanion = {
     enable = true;
+    package = pkgs.vimPlugins.codecompanion-nvim.overrideAttrs {
+      # NOTE: the following check fils on aarch64-darwin
+      # - codecompanion.providers.actions.fzf_lua
+      doCheck = false;
+    };
+    settings = {
+      display.action_palette.provider = "snacks";
+    };
   };
 }
