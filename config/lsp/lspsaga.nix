0d520b04758bde8b734f413db6271a09a88805b5
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-10-01 14:03:30 +0200

    fix: renamed options

--- config/lsp/lspsaga.nix
+++ config/lsp/lspsaga.nix
@@ -2,42 +2,44 @@
 {
   plugins.lspsaga = {
     enable = true;
-    beacon = {
-      enable = true;
-    };
-    ui = {
-      border = "rounded"; # One of none, single, double, rounded, solid, shadow
-      codeAction = "💡"; # Can be any symbol you want 💡
-    };
-    hover = {
-      openCmd = "!floorp"; # Choose your browser
-      openLink = "gx";
-    };
-    diagnostic = {
-      borderFollow = true;
-      diagnosticOnlyCurrent = false;
-      showCodeAction = true;
-    };
-    symbolInWinbar = {
-      enable = true; # Breadcrumbs
-    };
-    codeAction = {
-      extendGitSigns = true;
-      showServerName = true;
-      onlyInCursor = true;
-      numShortcut = true;
-      keys = {
-        exec = "<CR>";
-        quit = [
-          "<Esc>"
-          "q"
-        ];
+    settings = {
+      beacon = {
+        enable = true;
       };
-    };
-    lightbulb = {
-      enable = false;
-      sign = false;
-      virtualText = true;
+      ui = {
+        border = "rounded"; # One of none, single, double, rounded, solid, shadow
+        codeAction = "💡"; # Can be any symbol you want 💡
+      };
+      hover = {
+        openCmd = "!floorp"; # Choose your browser
+        openLink = "gx";
+      };
+      diagnostic = {
+        borderFollow = true;
+        diagnosticOnlyCurrent = false;
+        showCodeAction = true;
+      };
+      symbolInWinbar = {
+        enable = true; # Breadcrumbs
+      };
+      codeAction = {
+        extendGitSigns = true;
+        showServerName = true;
+        onlyInCursor = true;
+        numShortcut = true;
+        keys = {
+          exec = "<CR>";
+          quit = [
+            "<Esc>"
+            "q"
+          ];
+        };
+      };
+      lightbulb = {
+        enable = false;
+        sign = false;
+        virtualText = true;
+      };
     };
   };
 
