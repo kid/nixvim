fbd78095c40b48c010a37fc3919805eddf1748ea
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-10-04 11:05:20 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -5,10 +5,14 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixvim.url = "github:nix-community/nixvim";
+    gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
   };
 
   outputs =
     { flake-parts, ... }@inputs:
+    let
+      neovim-overlay = import ./nix/neovim-overlay.nix { inherit inputs; };
+    in
     flake-parts.lib.mkFlake { inherit inputs; } {
       # debug = true;
 
@@ -20,5 +24,28 @@
       imports = [
         ./nixvim.nix
       ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              neovim-overlay
+              inputs.gen-luarc.overlays.default
+            ];
+          };
+
+          devShells.default = pkgs.mkShell {
+            name = "nvim-devShell";
+            buildInputs = with pkgs; [
+              lua-language-server
+              nil
+              stylua
+              luajitPackages.luacheck
+              nvim-dev
+            ];
+          };
+        };
     };
 }
